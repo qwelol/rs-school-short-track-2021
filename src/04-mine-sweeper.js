386f6dba32361911eb98156c81d47a4473f03442
Alex <argentum.tm@gmail.com>
2021-04-07 20:42:42 +0300

    feat: implement minesweeper function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,31 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = new Array(matrix.length);
+  for (let i = 0; i < result.length; i++) {
+    result[i] = new Array(matrix[i].length);
+    result[i].fill(0);
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j]) {
+        if (i > 0) {
+          result[i - 1][j]++;
+          if (j > 0) result[i - 1][j - 1]++;
+          if (j < matrix[i].length) result[i - 1][j + 1]++;
+        }
+        if (j < matrix[i].length) result[i][j + 1]++;
+        if (j > 0) result[i][j - 1]++;
+        if (i < matrix.length) {
+          result[i + 1][j]++;
+          if (j > 0) result[i + 1][j - 1]++;
+          if (j < matrix[i].length) result[i + 1][j + 1]++;
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
