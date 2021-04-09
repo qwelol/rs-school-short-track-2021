b82f6f109222b0a0983c0750d01d5686ee5584c4
Alex <argentum.tm@gmail.com>
2021-04-09 19:29:34 +0300

    feat: implement encodeLine function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  if (str.length) {
+    let counter = 0;
+    for (let i = 0; i < str.length - 1; i++) {
+      const letter = str[i];
+      if (letter === str[i + 1]) {
+        counter++;
+      } else {
+        result += counter ? `${counter + 1}${letter}` : letter;
+        counter = 0;
+      }
+    }
+    result += counter ? `${counter + 1}${str[str.length - 1]}` : str[str.length - 1];
+  }
+  return result;
 }
 
 module.exports = encodeLine;
