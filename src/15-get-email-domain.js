fb8688bb091f54bcde97c112337dea0ed7325381
Alex <argentum.tm@gmail.com>
2021-04-09 22:06:23 +0300

    feat: implement getEmailDomain function

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const result = email.match(/(@{1})([a-z,A-Z,0-9,_,-]+.[a-z,A-Z,0-9,_,-]+)/);
+  return result ? result[2] : null;
 }
 
 module.exports = getEmailDomain;
